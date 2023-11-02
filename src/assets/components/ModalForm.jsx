2045a325162a3760d2d919924910d03018e7b395
Adi <121740959+Adi020@users.noreply.github.com>
2023-11-02 05:07:30 -0600

    Update ModalForm.jsx

--- src/assets/components/ModalForm.jsx
+++ src/assets/components/ModalForm.jsx
@@ -38,7 +38,7 @@ const ModalForm = ({
     >
       <form
         onSubmit={handleSubmit(submit)}
-        className={`${isDarkMode ? "" : "darkMode bg-neutral-800"} w-[280px] p-4 grid gap-6 relative`}
+        className={`${isDarkMode ? "whiteMode bg-white" : "darkMode bg-neutral-800"} w-[280px] p-4 grid gap-6 relative`}
       >
         <h3 className={`${isDarkMode ? "whiteMode" : "darkMode text-slate-100"}  font-bold text text-3xl`}>
           {isUserToUpdate ? "Editar Usuario" : "Nuevo Usuario"}
