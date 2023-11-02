e59fb002f5e1870c8091a355406b8a316ed71f58
Adi <121740959+Adi020@users.noreply.github.com>
2023-11-02 04:55:22 -0600

    Update UserList.jsx

--- src/assets/components/UserList.jsx
+++ src/assets/components/UserList.jsx
@@ -1,21 +1,26 @@
-import User from './User'
+import User from "./User";
 
-const UserList = ({users, deleteUser, changeShowModal, setIsUserToUpdate, isDarkMode}) => {
+const UserList = ({
+  users,
+  deleteUser,
+  changeShowModal,
+  setIsUserToUpdate,
+  isDarkMode,
+}) => {
   return (
     <section className="grid gap-x-16 gap-y-9 grid-cols-[repeat(auto-fill,_290px)] justify-center max-w-[1024px] mx-auto place-items-center pt-5 pb-10">
-        
-    {users.map((user) => (
-     <User 
-     key={user.id} 
-     user={user} 
-     deleteUser={deleteUser} 
-     changeShowModal={changeShowModal} 
-     setIsUserToUpdate={setIsUserToUpdate}
-     isDarkMode={isDarkMode}    
-     />))}
-
+      {users.map((user) => (
+        <User
+          key={user.id}
+          user={user}
+          deleteUser={deleteUser}
+          changeShowModal={changeShowModal}
+          setIsUserToUpdate={setIsUserToUpdate}
+          isDarkMode={isDarkMode}
+        />
+      ))}
     </section>
-  )
-}
+  );
+};
 
-export default UserList
+export default UserList;
