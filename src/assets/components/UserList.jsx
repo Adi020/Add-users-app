a033c71f6127784016fbf5b0722d021cd6259a30
Adi <121740959+Adi020@users.noreply.github.com>
2023-11-02 04:54:59 -0600

    Update UserList.jsx

--- src/assets/components/UserList.jsx
+++ src/assets/components/UserList.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
 import User from './User'
 
-const UserList = ({users, deleteUser, changeShowModal, setIsUserToUpdate}) => {
-  console.log(users)
+const UserList = ({users, deleteUser, changeShowModal, setIsUserToUpdate, isDarkMode}) => {
   return (
-    <section className=" grid gap-x-16 gap-y-9 grid-cols-[repeat(auto-fill,_290px)] justify-center max-w-[1024px] mx-auto place-items-center pt-5 pb-10">
+    <section className="grid gap-x-16 gap-y-9 grid-cols-[repeat(auto-fill,_290px)] justify-center max-w-[1024px] mx-auto place-items-center pt-5 pb-10">
         
     {users.map((user) => (
      <User 
@@ -13,7 +11,7 @@ const UserList = ({users, deleteUser, changeShowModal, setIsUserToUpdate}) => {
      deleteUser={deleteUser} 
      changeShowModal={changeShowModal} 
      setIsUserToUpdate={setIsUserToUpdate}
-        
+     isDarkMode={isDarkMode}    
      />))}
 
     </section>
