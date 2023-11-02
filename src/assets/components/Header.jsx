808a11b5bf11925b241382f84ff867a282762b3b
Adi <121740959+Adi020@users.noreply.github.com>
2023-11-02 04:53:30 -0600

    Update Header.jsx

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,16 +1,13 @@
-const Header = ({changeShowModal}) => {
-
+const Header = ({ changeShowModal, isDarkMode }) => {
   return (
-    <section className='flex justify-between p-10 px-9 max-w-[1200px] mx-auto'>
-       
+    <section className="flex justify-between p-10 px-9 max-w-[1200px] mx-auto">
+      <h1 className={`${isDarkMode ? "whiteMode" : "darkMode text-slate-100"} left-0 font-bold text-3xl `}>Usuarios</h1>
 
-          <h1 className=' left-0 font-bold text-3xl '>Usuarios</h1>
+      <button className="btn-primary" onClick={changeShowModal}>
+        <i className="bx bx-plus"></i>Crear nuevo usuario
+      </button>
+    </section>
+  );
+};
 
-          <button className='btn-primary' onClick={changeShowModal} ><i className='bx bx-plus'></i>Crear nuevo usuario</button>
-
-        
-      </section>
-  )
-}
-
-export default Header
+export default Header;
