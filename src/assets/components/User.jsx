332d5ad07be09cbc8a52527e9917633d790d4946
Adi <121740959+Adi020@users.noreply.github.com>
2023-11-02 04:54:22 -0600

    Update User.jsx

--- src/assets/components/User.jsx
+++ src/assets/components/User.jsx
@@ -1,33 +1,61 @@
-const User = ({user, deleteUser, changeShowModal, setIsUserToUpdate}) => {
+const User = ({
+  user,
+  deleteUser,
+  changeShowModal,
+  setIsUserToUpdate,
+  isDarkMode,
+}) => {
+  const handleUpperCase = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
   const handleClickDelete = () => {
-    deleteUser(user.id)
-  }
-  
+    deleteUser(user.id);
+  };
+
   const handleClickUpdate = () => {
-    changeShowModal()
-    setIsUserToUpdate(user)
-  }
+    changeShowModal();
+    setIsUserToUpdate(user);
+  };
 
   return (
-    <article className="relative shadow-lg w-80 h-56 px-3 border border-neutral-200 overflow-auto	">
-
-      <h4 className=" text-2xl py-3 font-bold	">{user.first_name} {user.last_name} </h4>
+    <article className={`${isDarkMode ? "border-neutral-200" : "border-neutral-500"} relative shadow-lg w-80 h-56 px-3 border overflow-auto`}>
+      <h4
+        className={`text-2xl py-3 font-bold ${isDarkMode ? "whiteMode" : "darkMode text-slate-100"}`}
+      >
+        {handleUpperCase(user.first_name)} {handleUpperCase(user.last_name)}
+      </h4>
       <div>
-        <h5 className=" text-neutral-200">Correo</h5>
-        <span>{user.email}</span>
+        <h5 className=" text-neutral-300">Correo:</h5>
+        <span className={`${isDarkMode ? "whiteMode" : "darkMode text-slate-100"}`}>{user.email}</span>
       </div>
 
       <div>
-        <h5 className=" text-neutral-200">Cumpleaños</h5>
-        <span><i className='bx bxs-gift'></i>{user.birthday || "Sin fecha"}</span>   
+        <h5 className="text-neutral-300">Cumpleaños:</h5>
+        <span className={`${isDarkMode ? "whiteMode" : "darkMode text-slate-100"}`}>
+          <i className="bx bxs-gift"></i>
+          {user.birthday || "Sin fecha"}
+        </span>
       </div>
+      <div className="absolute right-3 bottom-5">
+        <div className="flex gap-2">
+          <button
+            className="bg-red-500 flex justify-center items-center text-2xl text-gray-700 hover:bg-red-600 rounded-md w-8 h-8 border border-zinc-400"
+            onClick={handleClickDelete}
+          >
+            <i className="bx bxs-trash-alt"></i>
+          </button>
 
-      <button className="absolute bg-red-500 rounded-md h-8 w-8 right-12 bottom-5 text-gray-50 " onClick={handleClickDelete}><i className='bx bxs-trash-alt'></i></button>
-      
-      <button className="h-8 w-8 absolute right-2 bottom-5 rounded-md bg-gray-200 text-gray-100 border-zinc-500" onClick={handleClickUpdate}><i className='bx bxs-edit-alt' ></i></button>
+          <button
+            className="bg-gray-200 flex justify-center items-center text-2xl text-gray-700 hover:bg-gray-300 rounded-md w-8 h-8 border border-zinc-400"
+            onClick={handleClickUpdate}
+          >
+            <i className="bx bxs-edit-alt"></i>
+          </button>
+        </div>
+      </div>
     </article>
-  )
-}
+  );
+};
 
-export default User
+export default User;
