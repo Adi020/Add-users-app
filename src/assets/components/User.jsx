458ce78ba872d3b4e2be1fe688f144b47239ec4d
Adi <121740959+Adi020@users.noreply.github.com>
2023-11-02 20:34:02 -0600

    Update User.jsx

--- src/assets/components/User.jsx
+++ src/assets/components/User.jsx
@@ -19,20 +19,32 @@ const User = ({
   };
 
   return (
-    <article className={`${isDarkMode ? "border-neutral-200" : "border-neutral-500"} relative shadow-lg w-80 h-56 px-3 border overflow-auto`}>
+    <article
+      className={`${
+        isDarkMode ? "border-neutral-200" : "border-neutral-500"
+      } relative shadow-lg w-80 h-56 px-3 border overflow-auto`}
+    >
       <h4
-        className={`text-2xl py-3 font-bold ${isDarkMode ? "whiteMode" : "darkMode text-slate-100"}`}
+        className={`text-2xl py-3 font-bold ${
+          isDarkMode ? "whiteMode" : "darkMode text-slate-100"
+        }`}
       >
         {handleUpperCase(user.first_name)} {handleUpperCase(user.last_name)}
       </h4>
       <div>
         <h5 className=" text-neutral-300">Correo:</h5>
-        <span className={`${isDarkMode ? "whiteMode" : "darkMode text-slate-100"}`}>{user.email}</span>
+        <span
+          className={`${isDarkMode ? "whiteMode" : "darkMode text-slate-100"}`}
+        >
+          {user.email}
+        </span>
       </div>
 
       <div>
         <h5 className="text-neutral-300">Cumpleaños:</h5>
-        <span className={`${isDarkMode ? "whiteMode" : "darkMode text-slate-100"}`}>
+        <span
+          className={`${isDarkMode ? "whiteMode" : "darkMode text-slate-100"}`}
+        >
           <i className="bx bxs-gift"></i>
           {user.birthday || "Sin fecha"}
         </span>
@@ -40,14 +52,14 @@ const User = ({
       <div className="absolute right-3 bottom-5">
         <div className="flex gap-2">
           <button
-            className="bg-red-500 flex justify-center items-center text-2xl text-gray-700 hover:bg-red-600 rounded-md w-8 h-8 border border-zinc-400"
+            className="bg-red-500 flex justify-center items-center text-2xl text-gray-800 hover:bg-red-600 rounded-md w-8 h-8 border border-zinc-500"
             onClick={handleClickDelete}
           >
             <i className="bx bxs-trash-alt"></i>
           </button>
 
           <button
-            className="bg-gray-200 flex justify-center items-center text-2xl text-gray-700 hover:bg-gray-300 rounded-md w-8 h-8 border border-zinc-400"
+            className="bg-gray-200 flex justify-center items-center text-2xl text-gray-800 hover:bg-gray-300 rounded-md w-8 h-8 border border-zinc-500"
             onClick={handleClickUpdate}
           >
             <i className="bx bxs-edit-alt"></i>
