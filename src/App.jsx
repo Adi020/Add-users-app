a414d578463b5a8bd004865afd17a4410aa93f27
Adi <121740959+Adi020@users.noreply.github.com>
2024-08-26 12:31:06 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./assets/components/Header";
 import axios from "axios";
 import UserList from "./assets/components/UserList";
 
-const BASE_URL = "https://users-crud.academlo.tech";
+const BASE_URL = "https://users-crud-4fxo.onrender.com";
 
 const DEFAULT_VALUES = {
   birthday: "",
